ab161a782baad48565ac2fdd08ac6fef8b45e332
Roma Fominih <roma811@icloud.com>
2025-04-23 20:19:15 +0300

    Enhance logger middleware with response time tracking and status-based logging levels

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -2,6 +2,14 @@ import { logger } from '@events-project/common';
 import { Request, Response, NextFunction } from 'express';
 
 export const loggerMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  logger.debug(`${req.method} ${req.url}`);
+  const start = new Date();
+  res.on('finish', () => {
+    const duration = new Date().getTime() - start.getTime();
+    const level =
+      res.statusCode >= 400 ? 'error' : res.statusCode >= 200 && duration > 300 ? 'warn' : 'info';
+    logger[level](
+      `[${start.toISOString()}] ${req.method} ${req.url} ${res.statusCode} ${duration}ms`
+    );
+  });
   next();
 };
